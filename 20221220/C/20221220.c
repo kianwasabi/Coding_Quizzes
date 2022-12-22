9b149f51bca3940f99400f30353996be664e8b8a
kianwasabi <55065075+kianwasabi@users.noreply.github.com>
2022-12-22 13:17:08 +0100

    20221222
    -finished

--- 20221220/C/20221220.c
+++ 20221220/C/20221220.c
@@ -1,102 +1,64 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
-char str1[]= "Happy Birthday Brian";char str2[]= "BRaIN day th Happy BiR";
-
-int compare_char_array(char *arr1, char *arr2)
+int lowercase_replace_spaces_in_char_array(char *arr)
 {
-    //Source: https://stackoverflow.com/questions/27495346/compraring-char-array-without-using-strcmp-in-c
-    int i;
-    int response = 0;
-    i = 0;
-    while(arr1[i] == arr2[i] && response == 0 )
-    {
-        if(arr1[i] == '\0' || arr2[i] == '\0'){
-            response = 1;
+    int len_arr = strlen(arr);
+    int i,j=0;
+    for (i=0;i<=len_arr;i++)
+    { 
+        if (arr[i] ==' ')
+        {
+            for(j=i;j<len_arr;j++)
+            {
+                arr[j] = arr[j+1];
+            }
+            len_arr--;
         }
-        i++;
+        arr[i] = tolower(arr[i]); 
     }
-    return response;
+    return len_arr; 
+}
+
+void sort_char_array(int len_arr,char* arr)
+{
+    int i,j=0;
+    for(i=0;i<len_arr-1;i++)
+        {
+            for (j = i + 1; j < len_arr; j++)
+            {
+                if(arr[j]>arr[i])
+                {
+                    char arrtemp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = arrtemp;
+                }
+            }
+        }
 }
 
 int main()
 {
-    printf(" '%s' and  '%s' are\n",str1,str2);
-
-    //lowercase & replace spaces in char arrays
-    int len_str1 = sizeof(str1)/sizeof(str1[0]);
-    int len_str2 = sizeof(str2)/sizeof(str2[0]);
-    printf("%i, %i \n",len_str1,len_str2);
-    int i,j=0;
-    for (i=0;i<=len_str1;i++)
-    { 
-        if (str1[i] ==' ')
-        {
-            for(j=i;j<len_str1;j++)
-            {
-                str1[j] = str1[j+1];
-            }
-            len_str1--;
-        }
-        str1[i] = tolower(str1[i]); 
-    }
-
-    for (i=0;i<=len_str2;i++)
-    { 
-        if (str2[i] ==' ')
-        {
-            for(j=i;j<len_str2;j++)
-            {
-                str2[j] = str2[j+1];
-            }
-            len_str2--;
-        }
-        str2[i] = tolower(str2[i]); 
-    }
-
+    char str1[]= "Happy Birthday Brian";
+    char str2[]= "dayBirth Hap iaNBr py";
     printf(" '%s' and  '%s'\n",str1,str2);
-    printf("%i, %i \n",len_str1,len_str2);
-    //Check if strings are the same length and sorted strings are the same
-    if(len_str1!=len_str2){
-        printf("not anagrams. Strings are not the same length.");
+    //lowercase & replace spaces in char arrays
+    int len_str1 = lowercase_replace_spaces_in_char_array(&str1[0]);
+    int len_str2 = lowercase_replace_spaces_in_char_array(&str2[0]);
+    //sort char arrays
+    sort_char_array(len_str1,&str1[0]);
+    sort_char_array(len_str2,&str2[0]);
+    //Check if char arrays are the same length and sorted content is the same
+    if((len_str1==len_str2)&&(strcmp(str1,str2)==0)){
+        printf("are anagrams.");
     }else{
-        //sort first char array
-        for(i=0;i<len_str1-1;i++)
-        {
-            for (j = i + 1; j < len_str1; j++)
-            {
-                if(str1[j]>str1[i])
-                {
-                    char arrtemp = str1[i];
-                    str1[i] = str1[j];
-                    str1[j] = arrtemp;
-                }
-            }
+        printf("are not anagrams.");
         }
-        //sort second char array
-        for(i=0;i<len_str2-1;i++)
-        {
-            for (j = i + 1; j < len_str2; j++)
-            {
-                if(str2[j]>str2[i])
-                {
-                    char arrtemp = str2[i];
-                    str2[i] = str2[j];
-                    str2[j] = arrtemp;
-                }
-            }
-        }
-        printf(" '%s' and  '%s' are\n",str1,str2);
-        
-        if (compare_char_array(&str1,&str2) == 1) 
-        {
-            printf("anagrams.");
-        }
-        else
-        {
-            printf("not anagrams.");
-        }
-    }
     return 0;
 }
 
+/*
+    Comparing char array without using strcmp from string.h ->
+    https://stackoverflow.com/questions/27495346/compraring-char-array-without-using-strcmp-in-c
+*/
